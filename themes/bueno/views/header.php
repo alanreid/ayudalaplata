7d3e19cec089b02db72118c86d321a4be5be8a37
Alan Reid <mail@alanreid.com.ar>
2013-04-03 19:56:05 -0300

    Agregué número oficial de emergencia

--- themes/bueno/views/header.php
+++ themes/bueno/views/header.php
@@ -73,7 +73,7 @@
 			<?php echo $submit_btn; ?>
 			<!-- / submit incident -->
 
-      <h3 style="font-size:16px; margin-top:10px; float:right; clear:right">
+      <h3 style="font-size:16px; margin:21px -7px 0 0; float:right; clear:right">
         Número de emergencia: <span style="display:block; font-size:21px; text-align:center">0800 999 5959</span>
       </h3>
 
