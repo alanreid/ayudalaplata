12263cf936af8282c5f088911eec48c30b517d57
Alan Reid <mail@alanreid.com.ar>
2013-04-04 11:50:58 -0300

    Agregué like box

--- themes/default/views/main/timeline.php
+++ themes/default/views/main/timeline.php
@@ -10,3 +10,5 @@
 	<?php echo form::close(); ?>
 </div>
 <div id="graph" class="graph-holder"></div>
+
+<iframe src="//www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Fayudalaplata&amp;width=292&amp;height=395&amp;show_faces=false&amp;colorscheme=light&amp;stream=true&amp;border_color&amp;header=false&amp;appId=171847206299593" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:292px; height:395px;" allowTransparency="true"></iframe>
