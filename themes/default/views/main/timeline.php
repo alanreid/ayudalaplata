2c0ee987b411e93c61a2160f0cd759d6c93bd1f5
Alan Reid <mail@alanreid.com.ar>
2013-04-05 17:05:17 -0300

    Agregué el widget de twitter

--- themes/default/views/main/timeline.php
+++ themes/default/views/main/timeline.php
@@ -12,3 +12,8 @@
 <div id="graph" class="graph-holder"></div>
 
 <iframe src="//www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2Fayudalaplata&amp;width=575&amp;height=395&amp;show_faces=false&amp;colorscheme=light&amp;stream=true&amp;border_color&amp;header=false&amp;appId=171847206299593" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:575px; height:395px;" allowTransparency="true"></iframe>
+
+<div style="padding:30px">
+<a class="twitter-timeline" href="https://twitter.com/AyudaLaPlata" data-widget-id="320264256585076736">Tweets by @AyudaLaPlata</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+</div>
